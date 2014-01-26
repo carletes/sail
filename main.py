3c335a583ecdc3871b36b70119b42b19a9e2df55
Kass Schmitt <kass.schmitt@gmail.com>
2014-01-26 17:37:24 +0000

    implemented boat.move with some testing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import unittest
-
+from math import radians, cos, sin
 
 class Boat():
     def __init__(self, position, compass):
@@ -14,6 +14,11 @@ class Boat():
     def compass(self):
         return self._compass
 
+    def move(self,distance):
+        theta = radians(self._compass + 90)
+        d_x = distance * cos(theta)
+        d_y = distance * sin(theta)
+        self._position = (self._position[0] + d_x, self._position[1] + d_y)
 
 class BoatTest(unittest.TestCase):
     def test_create_boat(self):
@@ -21,6 +26,17 @@ class BoatTest(unittest.TestCase):
         self.assertEqual(boat.position,(30,30))
         self.assertEqual(boat.compass, 75)
 
+    def test_move_boat(self):
+        boat = Boat(position=(0,0), compass= 0)
+        boat.move(10)
+        self.assertAlmostEqual(boat.position[0],0)
+        self.assertAlmostEqual(boat.position[1],10)
+
+        boat.move(-10)
+        self.assertAlmostEqual(boat.position[0],0)
+        self.assertAlmostEqual(boat.position[1],0)        
+
+        
 def main():
     pass
 
