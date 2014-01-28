83598f6cf0998026ac322160d44fc8959e9cf68b
Carlos Valiente <carlos@pepelabs.net>
2014-01-28 21:20:26 +0000

    Environment: First cut.
    This commit implements a sketch of the ``move()`` function, which moves
    a boat according to the efect of several forces (like its engine, the
    wind, the tide ...).

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import logging
 import unittest
-from math import degrees, fmod, radians, cos, sin, sqrt, pi
+
+from math import degrees, fmod, radians, cos, sin, pi, sqrt
 from nearestkey import nearest_key
 
 
 class Boat():
+
+    log = logging.getLogger("Boat")
+
     def __init__(self, position, compass, polars):
         self._position = position
         # want 0 <= compass < 360
@@ -21,16 +26,20 @@ class Boat():
     def compass(self):
         return degrees(self._theta)
 
+    @property
+    def theta(self):
+        return self._theta
+
     def polars(self, windspeed, windangle):
         nearest_windspeed = nearest_key(self._polars,windspeed)
         nearest_windangle = nearest_key(self._polars[nearest_windspeed],windangle)
         return self._polars[nearest_windspeed][nearest_windangle]
 
-    def move(self,distance):
-        # TODO: check distance is positive, don't allow if not
-        d_x = distance * sin(self._theta)
-        d_y = distance * cos(self._theta)
-        self._position = (self._position[0] + d_x, self._position[1] + d_y)
+    def move(self, d_x, d_y):
+        new_position = (self._position[0] + d_x, self._position[1] + d_y)
+        self.log.debug("move(): Moving from %s to %s",
+                       self._position, new_position)
+        self._position = new_position
 
     def steer(self,degrees):
         # positive values for starboard, negative to port
@@ -51,16 +60,6 @@ class BoatTest(unittest.TestCase):
         self.assertEqual(boat.compass, 75)
         self.assertEqual(boat.polars(0,0),0)
 
-    def test_move_boat(self):
-        boat = Boat(position=(0,0), compass=0, polars={0:{0:0}})
-        boat.move(10)
-        self.assertAlmostEqual(boat.position[0],0)
-        self.assertAlmostEqual(boat.position[1],10)
-
-        boat.move(-10)
-        self.assertAlmostEqual(boat.position[0],0)
-        self.assertAlmostEqual(boat.position[1],0)        
-
     def test_steer_boat(self):
         boat = Boat(position=(0,0), compass=0, polars={0:{0:0}})
         boat.steer(90)
@@ -74,14 +73,6 @@ class BoatTest(unittest.TestCase):
         boat.steer(730)
         self.assertEqual(boat.compass,290)
 
-    def test_move_and_steer_boat(self):
-        boat = Boat(position=(0,0), compass=0, polars={0:{0:0}})
-        boat.steer(45)
-        boat.move(sqrt(200))
-        self.assertAlmostEqual(boat.position[0],10)        
-        self.assertAlmostEqual(boat.position[1],10)        
-        self.assertEqual(boat.compass,45)
-
     def test_get_polars(self):
         boat = Boat(position=(0,0), compass=0, polars={0:{0:0,15:0,30:0,180:0},5:{0:0,15:0.8,30:1,45:1.5,180:3},10:{0:0,15:1.3,30:1.7,180:5},15:{0:0,15:2,30:3,180:10}})
         self.assertEqual(boat.polars(0,0),0)
@@ -93,12 +84,110 @@ class BoatTest(unittest.TestCase):
         self.assertEqual(boat.polars(5,170),3)
 
 
-    
+class Force(object):
 
-def main():
-    pass
+    """Base class of things that make a boat move."""
 
+    def effect(self, boat, t):
+        """Applies this force to a boat at a given point in time.
+
+        Returns the effect, expressed as a vector ``(x, y)``, of applying this
+        force. Each component of the return value represents the number of
+        units to move in each axis.
+
+        """
+        raise NotImplementedError()
+
+
+class Engine(Force):
+
+    log = logging.getLogger("Engine")
+
+    def __init__(self, k):
+        """Initialise an engine with an arbitrary constant, representing the
+        magnitude of its effect vector.
+
+        """
+        # TODO: Pass an initial amount of fuel to the constructor.
+        self.k = k
+
+    def effect(self, boat, t):
+        theta = boat.theta
+        self.log.debug("effect(%g): Theta: %g", self.k, theta)
+        # TODO: Use a non-arbitrary speed, take into account remaining
+        # fuel, ...
+        ret = (self.k * sin(theta), self.k * cos(theta))
+        self.log.debug("effect(%g): Moving to (%s, %s)", self.k, ret[0], ret[1])
+        return ret
+
+
+class Wind(Force):
+
+    def __init__(self):
+        # TODO: Pass the path to a GRIB file with the values for the ``u`` and
+        # ``v`` components of the wind.
+        pass
+
+    def effect(self, boat, t):
+        x, y = boat.position
+        u, v = self.wind_at(x, y, t)
+        # TODO: Take into account the polars of the boat, and perhaps also the
+        # boat's compass. For now we just return an arbitrary multiple of the
+        # wind components.
+        #
+        # Also take in to account any other factor (polars?) that you might
+        # think of!
+        return (0.42 * u, 0.42 * v)
+
+    def wind_at(self, x, y, t):
+        """Returns the ``u`` and ``v`` components of the wind at coordinates
+        ``(x, y)``.
+
+        """
+        # TODO: Use data from a GRIB file instead.
+        return (10, 10)
+
+
+def move(boat, forces, t):
+    """Move a boat at a given point in time, taking into account all the
+    forces acting upon it.
+
+    """
+    new_x, new_y = 0, 0
+    for (d_x, d_y) in (f.effect(boat, t) for f in forces):
+        new_x += d_x
+        new_y += d_y
+    boat.move(new_x, new_y)
+
+
+class MoveTest(unittest.TestCase):
+
+    def test_engine(self):
+        boat = Boat(position=(0,0), compass=0, polars={0:{0:0}})
+        engine = Engine(10)
+        move(boat, [engine], 0)
+        self.assertAlmostEqual(boat.position[0],0)
+        self.assertAlmostEqual(boat.position[1],10)
+
+        engine = Engine(-10)
+        move(boat, [engine], 0)
+        self.assertAlmostEqual(boat.position[0],0)
+        self.assertAlmostEqual(boat.position[1],0)
+
+        engine = Engine(sqrt(200))
+        boat = Boat(position=(0,0), compass=0, polars={0:{0:0}})
+        boat.steer(45)
+        move(boat, [engine], 0)
+        self.assertAlmostEqual(boat.position[0],10)
+        self.assertAlmostEqual(boat.position[1],10)
+        self.assertEqual(boat.compass,45)
+
+    def test_wind(self):
+        # TODO: Implement!
+        pass
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s %(name)s %(message)s")
     unittest.main()
