3ac9b3d0a25f14fa6b9eb05a8bc3ef60f71246ac
Kass Schmitt <kass.schmitt@gmail.com>
2014-01-28 16:37:33 +0000

    Boat now initialised with dictionary of polars which give the theoretical boat speed for any combination of windspeed and windangle. If the exact key for one or both of these does not exist in the dict, the polars method use the nearest existing key for each. Later on we can add some interpolation or even possibly derive a function from the table values. nearest_key function used in polars method.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import unittest
 from math import degrees, fmod, radians, cos, sin, sqrt, pi
+from nearestkey import nearest_key
 
 
 class Boat():
-    def __init__(self, position, compass):
+    def __init__(self, position, compass, polars):
         self._position = position
         # want 0 <= compass < 360
         compass = fmod(compass, 360)
         if compass < 0:
             compass = 360 + compass
         self._theta = radians(compass)
+        self._polars = polars
 
     @property
     def position(self):
@@ -18,6 +20,11 @@ class Boat():
     @property
     def compass(self):
         return degrees(self._theta)
+
+    def polars(self, windspeed, windangle):
+        nearest_windspeed = nearest_key(self._polars,windspeed)
+        nearest_windangle = nearest_key(self._polars[nearest_windspeed],windangle)
+        return self._polars[nearest_windspeed][nearest_windangle]
 
     def move(self,distance):
         # TODO: check distance is positive, don't allow if not
@@ -39,12 +46,13 @@ class Boat():
 
 class BoatTest(unittest.TestCase):
     def test_create_boat(self):
-        boat = Boat(position=(30,30), compass=75)
+        boat = Boat(position=(30,30), compass=75, polars={0:{0:0}})
         self.assertEqual(boat.position,(30,30))
         self.assertEqual(boat.compass, 75)
+        self.assertEqual(boat.polars(0,0),0)
 
     def test_move_boat(self):
-        boat = Boat(position=(0,0), compass=0)
+        boat = Boat(position=(0,0), compass=0, polars={0:{0:0}})
         boat.move(10)
         self.assertAlmostEqual(boat.position[0],0)
         self.assertAlmostEqual(boat.position[1],10)
@@ -54,7 +62,7 @@ class BoatTest(unittest.TestCase):
         self.assertAlmostEqual(boat.position[1],0)        
 
     def test_steer_boat(self):
-        boat = Boat(position=(0,0), compass=0)
+        boat = Boat(position=(0,0), compass=0, polars={0:{0:0}})
         boat.steer(90)
         self.assertEqual(boat.compass, 90)
         boat.steer(-10)
@@ -67,12 +75,23 @@ class BoatTest(unittest.TestCase):
         self.assertEqual(boat.compass,290)
 
     def test_move_and_steer_boat(self):
-        boat = Boat(position=(0,0), compass=0)
+        boat = Boat(position=(0,0), compass=0, polars={0:{0:0}})
         boat.steer(45)
         boat.move(sqrt(200))
         self.assertAlmostEqual(boat.position[0],10)        
         self.assertAlmostEqual(boat.position[1],10)        
         self.assertEqual(boat.compass,45)
+
+    def test_get_polars(self):
+        boat = Boat(position=(0,0), compass=0, polars={0:{0:0,15:0,30:0,180:0},5:{0:0,15:0.8,30:1,45:1.5,180:3},10:{0:0,15:1.3,30:1.7,180:5},15:{0:0,15:2,30:3,180:10}})
+        self.assertEqual(boat.polars(0,0),0)
+        self.assertEqual(boat.polars(0,180),0)
+        self.assertEqual(boat.polars(5,180),3)
+        self.assertEqual(boat.polars(6,30),1)
+        self.assertEqual(boat.polars(6,170),3)
+        self.assertEqual(boat.polars(10,180),5)
+        self.assertEqual(boat.polars(5,170),3)
+
 
     
 
---
+++ nearestkey.py
@@ -0,0 +1,61 @@
+import unittest
+
+def nearest_key(dict,target):
+    """
+    'Given a dict in which all keys are numeric, return the key closest to a
+    supplied target.'
+
+    Example:
+
+    >>> testdict = {0:0, 10:0, 'foo':'bar', 20:0, 30:0, 40:0}
+    >>> nearest_key(testdict,-1)
+    0
+    >>> nearest_key(testdict,0)
+    0
+    >>> nearest_key(testdict,1)
+    0
+    >>> nearest_key(testdict,5)
+    0
+    >>> nearest_key(testdict,6)
+    10
+    >>> nearest_key(testdict,45)
+    40
+    >>>
+
+    """
+    if target in dict.keys():
+        return target
+    leftneighbour = None
+    rightneighbour = None
+    for i in dict.keys():
+        if not isinstance(i, (int, long, float, complex)): continue
+        if i < target:
+            if leftneighbour == None:
+                leftneighbour = i
+            elif leftneighbour < i:
+                leftneighbour = i
+        elif i > target:
+            if rightneighbour == None:
+                rightneighbour = i
+            elif rightneighbour > i:
+                rightneighbour = i            
+    if leftneighbour == rightneighbour:
+        return leftneighbour
+    if leftneighbour == None:
+        return rightneighbour
+    elif rightneighbour == None:
+        return leftneighbour
+    elif (target - leftneighbour) <= (rightneighbour - target):
+        return leftneighbour
+    else:
+        return rightneighbour
+
+
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+ 
+if __name__ == "__main__":
+    _test()
